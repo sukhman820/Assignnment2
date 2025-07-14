70f2dffb2b0bfb488aa7231a8a15e7391392a30e
sukhman820 <ssukhmanpreet820@gmail.com>
2025-07-14 10:35:42 -0400

    Delete models/books.js

--- models/books.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const bookSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  author: { type: String, required: true },
-  borrowed: { type: Boolean, default: false }
-});
-module.exports = mongoose.model('Book', bookSchema);
